ae7d8e21241f5968ea8d6079e642957f969ca706
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-05-05 11:27:03 +0300

    added Next word button that just reloads the new word and check button that check the char with the word

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,14 @@
 let fortune = document.querySelector('#fortune-teller')
 let guessWord = document.querySelector('.word-display')
 let nextTurn = document.querySelector('#next-turn')
+let input = document.querySelector('#input')
+let nxtwrdBtn = document.querySelector('#nextWord')
+let checkBtn = document.querySelector('#check')
 let currentWord = ''
+let inputVal = ''
+let wordArray = []
+let checkArr = []
+
 ///////////////functions////////////////////
 const gameFortune = () => {
   let Random = Math.floor(Math.random() * 100)
@@ -18,12 +25,36 @@ const randomWord = () => {
     .map((word) => `<li class = "letter"></li>`)
     .join('')
   document.querySelector('.hint-text b').innerText = hint
+  wordArray = [...currentWord]
+  console.log(wordArray)
+  inputVal = input.value.toLowerCase()
+  console.log(inputVal)
 }
 
-const winCheck = () => {}
+const winCheck = () => {
+  wordArray = [...currentWord]
+  console.log(wordArray)
+  inputVal = input.value.toLowerCase()
+  console.log(inputVal)
+  wordArray.forEach((word, index) => {
+    if (word === inputVal) {
+      console.log('happy')
+      checkArr.push(inputVal)
+      guessWord.querySelectorAll('li')[index].innerText = inputVal
+    }
+  })
+}
 
 //////////////Event Listners///////////////////////////
 nextTurn.addEventListener('click', () => {
   gameFortune()
   randomWord()
 })
+nxtwrdBtn.addEventListener('click', () => {
+  randomWord()
+})
+
+checkBtn.addEventListener('click', () => {
+  winCheck()
+})
+gameFortune()
