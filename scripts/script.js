627b76fbd88e8165cc3e7019cf5c8077a7878cf1
User <osamamohammad343@gmail.com>
2024-05-05 20:30:50 +0300

    added the post MVP animation of changing numbers and a pop up box when the game overs

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,8 @@ let input = document.querySelector('#input')
 let scoreValue = document.querySelector('#score-value')
 let resetBtn = document.querySelector('#reset-btn')
 let wrng = document.querySelector('#wrong-guesses')
+let popup = document.querySelector("#game-over-popup")
+let playAgain = document.querySelector("#playAgain")
 let currentWord,
   inputVal,
   Random,
@@ -15,8 +17,9 @@ let checkArr = []
 let score = 0
 let diff = 0
 let time = 20
-let wrongguess = 0
+let wrongGuess = 0
 let correct = false
+let maxGuesses = 6
 
 ///////////////functions////////////////////
 
@@ -28,12 +31,14 @@ const gameFortune = () => {
 const animateFortune = (finalNumber) => {
   let currentNumber = 0;
   let animationInterval = setInterval(() => {
-    currentNumber = Math.floor(Math.random() * 100); // Range of numbers
+    currentNumber = Math.floor(Math.random() * 100);
     fortune.innerText = currentNumber;
+    nextTurn.disabled= true
     if (currentNumber === finalNumber) {
+      nextTurn.disabled= false
       clearInterval(animationInterval);
     }
-  }, 50); 
+  }, 30); 
 };
 
 const randomWord = () => {
@@ -46,6 +51,20 @@ const randomWord = () => {
   document.querySelector('.hint-text b').innerText = hint
   wordArray = [...currentWord]
   console.log(wordArray)
+}
+
+const gameOver=(won)=>{
+  popup.style.display  = "block";
+  if (won){
+    popup.innerText = `You have won the Game and your Scores are ${score}`
+  }
+  else {
+    popup.innerText = `the correct word was ${currentWord}`
+  }
+  playAgain.addEventListener('click', ()=>{
+    popup.style.display  = "none";
+  })
+
 }
 
 const gamePlay = () => {
@@ -68,18 +87,20 @@ const gamePlay = () => {
     }
   })
   if (!correct) {
-    wrongguess++
+    wrongGuess++
 
-    wrng.innerText = wrongguess
+    wrng.innerText = `${wrongGuess} / ${maxGuesses}`
   }
-  ///Insert a game over function
-
   score += Random * diff
   scoreValue.innerText = score
   Random = Math.floor(Math.random() * 100)
 
-  animateFortune(Random)
   diff = 0
+  input.value = ''
+  if(wrongGuess === maxGuesses) return gameOver(false);
+  if(checkArr.length === wordArray.length) return gameOver(true);
+
+  animateFortune(Random)
 }
 
 const reset = () => {
@@ -104,3 +125,7 @@ nextTurn.addEventListener('click', () => {
 
 randomWord()
 gameFortune()
+
+
+
+////the button is not displaying in popup
