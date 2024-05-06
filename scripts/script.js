0c768c48427c4140361322f07ce460e77d6d2d6f
User <osamamohammad343@gmail.com>
2024-05-06 23:36:34 +0300

    added a solve function and its popup fucntion and tested the whole game and made other small modififcations

--- scripts/script.js
+++ scripts/script.js
@@ -10,17 +10,21 @@ let popup = document.querySelector('#game-over-popup')
 let msg = document.querySelector('#msg-disp')
 let playAgain = document.querySelector('#playAgain')
 let body = document.querySelector('body')
-let errorpop = document.querySelector('#empty-popup')
-let errmsg = document.querySelector('#error-disp')
-let tryAgain = document.querySelector('#tryAgain')
+const solvePop = document.querySelector("#solve-popup")
+const solveInput = document.querySelector("#solve-input")
+const checkBtn = document.querySelector("#check-btn")
+const solveBtn= document.querySelector("#solvebtn")
+const redobtn= document.querySelector("#redo-btn")
 let currentWord,
   inputVal,
   Random = ''
 let wordArray = []
 let checkArr = []
+let solveArr =[]
 let score = 0
 let diff = 0
 let wrongGuess = 0
+let done= false 
 let correct = false
 let maxGuesses = 6
 let vowels = ['a', 'e', 'i', 'o', 'u']
@@ -28,20 +32,18 @@ let vowels = ['a', 'e', 'i', 'o', 'u']
 ///////////////functions////////////////////
 body.onload = () => {
   randomWord()
-  gameFortune()
+  animateFortune()
+  wrng.innerText = `${wrongGuess} / ${maxGuesses}`
+  scoreValue.innerText = score
 }
-const gameFortune = () => {
-  Random = Math.floor(Math.random() * 100)
-
-  fortune.innerText = Random
-}
-const animateFortune = (finalNumber) => {
+const animateFortune = () => {
   let currentNumber = 0
+  Random = Math.floor(Math.random() * 100)
   let animationInterval = setInterval(() => {
     currentNumber = Math.floor(Math.random() * 100)
     fortune.innerText = currentNumber
     nextTurn.disabled = true
-    if (currentNumber === finalNumber) {
+    if (currentNumber === Random) {
       nextTurn.disabled = false
       clearInterval(animationInterval)
     }
@@ -49,9 +51,10 @@ const animateFortune = (finalNumber) => {
 }
 
 const randomWord = async () => {
-  let test1 = await axios.get(`https://random-word-api.herokuapp.com/word`)
-  console.log(test1)
-  currentWord = test1.data[0]
+  let test1 = await axios.get('https://api.api-ninjas.com/v1/randomword', {
+    headers: { 'X-Api-Key': 'FLozrBrtj7nfy0JE0CItCQ==ikziNculhRtgSryP' }
+  })
+  currentWord = test1.data.word
   console.log(currentWord)
   guessWord.innerHTML = currentWord
     .split('')
@@ -61,20 +64,17 @@ const randomWord = async () => {
   let test = await axios.get(
     `https://api.dictionaryapi.dev/api/v2/entries/en/${currentWord}`
   )
-  console.log(test)
   hint = test.data[0].meanings[0].definitions[0].definition
-
+  console.log(hint)
   document.querySelector('.hint-text b').innerText = hint
   wordArray = [...currentWord]
   console.log(wordArray)
   checkArr = []
 }
 
-const gameOver = (won) => {
+const gameOver = () => {
   popup.style.display = 'block'
   playAgain.style.display = 'block'
-  popup.style.opacity = '1'
-  playAgain.style.opacity = '1'
   nextTurn.disabled = true
   resetBtn.disabled = true
   msg.innerText = `the correct word was ${currentWord} and Your scores are ${score}`
@@ -84,18 +84,13 @@ const gamePlay = () => {
   inputVal = input.value.toLowerCase()
   console.log(inputVal)
   if (input.value.trim() === '') {
-    errorpop.style.display = 'block'
-    tryAgain.style.display = 'block'
-    errmsg.innerText = 'No character entered. Please enter a letter.'
     return
   }
-
   wordArray.forEach((word, index) => {
     if (word === inputVal) {
       correct = true
       checkArr.push(inputVal)
       guessWord.querySelectorAll('li')[index].innerText = inputVal
-
       input.value = ''
       diff++
     }
@@ -106,27 +101,17 @@ const gamePlay = () => {
     wrng.innerText = `${wrongGuess} / ${maxGuesses}`
   }
   score += Random * diff
-  Random = Math.floor(Math.random() * 100)
-
   vowels.forEach((vowel) => {
     if (inputVal === vowel) {
-      if (score > 25) {
-        score = score - 25
-      } else {
-        input.value = ''
-        console.log("vowel can't be used")
-      }
-    }
+       score = score - 25}
   })
+
   scoreValue.innerText = score
-  Random = Math.floor(Math.random() * 100)
   diff = 0
   input.value = ''
   if (wrongGuess === maxGuesses) {
     return gameOver()
   }
-
-  animateFortune(Random)
   correct = false
 }
 
@@ -138,7 +123,7 @@ const reset = () => {
   wrongGuess = 0
   correct = false
   randomWord()
-  gamePlay()
+  animateFortune()
 }
 
 //////////////Event Listners///////////////////////////
@@ -146,26 +131,53 @@ const reset = () => {
 nextTurn.addEventListener('click', () => {
   if (checkArr.length != wordArray.length && checkArr.length > 0) {
     gamePlay()
+    animateFortune()
   } else if (checkArr.length === 0) {
     gamePlay()
+    animateFortune()
   } else if (checkArr.length === wordArray.length) {
-    console.log(checkArr.length)
     gamePlay()
     randomWord()
   }
 })
 
+
 playAgain.addEventListener('click', () => {
   popup.style.display = 'none'
   playAgain.style.display = 'none'
-  body.style.opacity = '1'
   reset()
-  randomWord()
-  gamePlay()
   wrng.innerText = `${wrongGuess} / ${maxGuesses}`
   scoreValue.innerText = score
+  nextTurn.disabled=false
+  resetBtn.disabled=false
 })
 
 resetBtn.addEventListener('click', () => {
   reset()
+})
+
+checkBtn.addEventListener('click', ()=>{
+ 
+  
+    if (currentWord===solveInput.value){
+      score += Random * 50
+      randomWord()
+      gamePlay()
+      animateFortune()
+    }
+    else {
+      score=0;
+      gameOver()
+    }
+    scoreValue.innerText = score
+    solvePop.style.display="none"
+})
+solveBtn.addEventListener('click', ()=>{
+  solvePop.style.display = "block"
+})
+
+redobtn.addEventListener('click', ()=>{
+  animateFortune()
+  randomWord()
+
 })
